82f9a0f26d4d49ccbb8836192a90af66582026d0
Pavel <ku2zovpavel@gmail.com>
2022-03-02 19:08:06 +0300

    Add functions

--- Yellow Belt/Week 2/bus_station_starter.cpp
+++ Yellow Belt/Week 2/bus_station_starter.cpp
@@ -26,8 +26,8 @@ istream& operator >> (istream& is, Query& q) {
     is >> operation_code >> operation_code;
     if (operation_code == "NEW_BUS")
     {
-        string bus;
-        is >> bus;
+        q.type = QueryType::NewBus;
+        is >> q.bus;
         int stop_count;
         is >> stop_count;
         q.stops.resize(stop_count);
@@ -35,53 +35,137 @@ istream& operator >> (istream& is, Query& q) {
             cin >> q.stops[i];
         }
     }
+    else if (operation_code == "BUSES_FOR_STOP")
+    {
+        q.type = QueryType::BusesForStop;
+        is >> q.stop;
+    }
+    else if (operation_code == "STOPS_FOR_BUS")
+    {
+        q.type = QueryType::StopsForBus;
+        is >> q.bus;
+    }
+    else if (operation_code == "ALL_BUSES")
+    {
+        q.type = QueryType::AllBuses; 
+    }
     return is;
 }
 
 struct BusesForStopResponse {
   // Наполните полями эту структуру
+     vector<string> buses;
 };
 
 ostream& operator << (ostream& os, const BusesForStopResponse& r) {
-  // Реализуйте эту функцию
-  return os;
+    // Реализуйте эту функцию
+    
+    if (r.buses.size() != 0)
+    {
+        for(int i=0;i<r.buses.size();i++)
+        {
+            if(i < r.buses.size()-1)
+            {
+                os << r.buses[i] << " ";    
+            }
+            else
+            {
+                os << r.buses[i] << endl; 
+            }
+        }
+    }
+    else
+    {
+        os << "No stop" << endl;
+    }
+
+    return os;
 }
 
 struct StopsForBusResponse {
-  // Наполните полями эту структуру
+    // Наполните полями эту структуру
+    map<string, vector<string>> stops;
 };
 
 ostream& operator << (ostream& os, const StopsForBusResponse& r) {
-  // Реализуйте эту функцию
-  return os;
+    // Реализуйте эту функцию
+    if (r.stops.size() != 0)
+    {
+        for(auto& stop : r.stops)
+        {
+            os << "Stop " << stop.first << ": ";
+            
+            if (stop.second.size() != 0)
+            {
+                for(auto& bus : stop.second)
+                os << bus << " ";
+            }
+            else
+            {
+                os << "no interchange" << endl;
+            }
+            os << endl;
+        }
+    }
+    return os;
 }
 
 struct AllBusesResponse {
-  // Наполните полями эту структуру
+    // Наполните полями эту структуру
+    map<string, vector<string>> buses;
 };
 
 ostream& operator << (ostream& os, const AllBusesResponse& r) {
-  // Реализуйте эту функцию
-  return os;
+    // Реализуйте эту функцию
+    if (r.buses.size() != 0)
+    {
+        for(auto& bus : r.buses)
+        {
+            os << "Bus " << bus.first << ": ";
+            for(auto& stop : bus.second)
+            {
+                os << stop << " ";
+            }                
+            os << endl;
+        }
+    }
+    else
+    {
+        os << "No buses" << endl;
+    }
+    return os;
 }
 
 class BusManager {
 public:
-  void AddBus(const string& bus, const vector<string>& stops) {
+void AddBus(const string& bus, const vector<string>& stops) 
+{
     // Реализуйте этот метод
-  }
+    buses_to_stops[bus] = stops;
+    
+    for (const string& stop : stops) 
+    {
+        stops_to_buses[stop].push_back(bus);
+    }
+}
 
-  BusesForStopResponse GetBusesForStop(const string& stop) const {
+BusesForStopResponse GetBusesForStop(const string& stop) const 
+{
     // Реализуйте этот метод
-  }
+    return stops_to_buses.at(stop);
+}
 
-  StopsForBusResponse GetStopsForBus(const string& bus) const {
-    // Реализуйте этот метод
-  }
+StopsForBusResponse GetStopsForBus(const string& bus) const 
+{
+    // Реализуйте этот метод  
+    return buses_to_stops.at(bus);
+}
 
-  AllBusesResponse GetAllBuses() const {
+AllBusesResponse GetAllBuses() const 
+{
     // Реализуйте этот метод
-  }
+    return buses_to_stops;
+}
 private:
     map<string, vector<string>> buses_to_stops, stops_to_buses;
 };
